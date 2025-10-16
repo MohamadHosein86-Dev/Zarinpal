227c7264bebc065405a4bfe0994ab0e4cbb6aec6
MohamadHosein86-Dev <mohamadhoseinsafaryan86@gmail.com>
2025-10-16 22:12:48 +0330

    feat(page): add team section for about page with member cards and filter

--- app/(page)/about/page.tsx
+++ app/(page)/about/page.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import HeroSection from "./components/HeroSection";
 import MissionSection from "./components/MissionSection";
 import CompanyValues from "./components/CompanyValues";
+import Timeline from "./components/Timeline";
+import TeamSection from "./components/TeamSection";
 
 export default function AboutPage() {
   return (
@@ -9,6 +11,8 @@ export default function AboutPage() {
       <HeroSection />
       <MissionSection />
       <CompanyValues />
+      <Timeline />
+      <TeamSection />
     </section>
   );
 }
