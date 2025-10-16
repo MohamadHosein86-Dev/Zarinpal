50b02bde1e0a96285ec4bf59c6fba0d4d5ebf913
MohamadHosein86-Dev <mohamadhoseinsafaryan86@gmail.com>
2025-10-16 22:09:00 +0330

    feat(page): add company values section for about page with animated cards

--- app/(page)/about/page.tsx
+++ app/(page)/about/page.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import HeroSection from "./components/HeroSection";
 import MissionSection from "./components/MissionSection";
+import CompanyValues from "./components/CompanyValues";
 
 export default function AboutPage() {
   return (
     <section>
       <HeroSection />
       <MissionSection />
+      <CompanyValues />
     </section>
   );
 }
