efd98c7671173eba087b21267dc03115878f62cd
MohamadHosein86-Dev <mohamadhoseinsafaryan86@gmail.com>
2025-10-22 18:18:57 +0330

    feat(page): add user sidebar component with animated menu and profile section

--- app/lib/helpfunc.ts
+++ app/lib/helpfunc.ts
@@ -1,4 +1,7 @@
 export const formatCurrency = (amount: number) => {
-    return new Intl.NumberFormat("fa-IR").format(amount) + " تومان";
-  };
-  
+  return new Intl.NumberFormat("fa-IR").format(amount) + " تومان";
+};
+export function toggleMobileNavbar() {
+    document.querySelector('.navigation')?.classList.toggle('menu-active')
+    document.querySelector('.mobile-overlay')?.classList.toggle('show')
+}
