bb6fe851728181be37be53396761d4a7cec1b229
MohamadHosein86-Dev <mohamadhoseinsafaryan86@gmail.com>
2025-11-03 15:57:45 +0330

    feat(api): add user registration endpoint with validation, hashing, and duplicate handling

---
+++ app/api/register/route.ts
@@ -0,0 +1,40 @@
+import { NextResponse } from "next/server";
+import bcrypt from "bcrypt";
+import { prisma } from "@/app/lib/prisma";
+
+export async function POST(req: Request) {
+  try {
+    const { businessName, businessType, lastName, firstName, email, phone, password, role = "USER" } = await req.json();
+
+    if (!businessName || !businessType || !lastName || !firstName || !email || !phone || !password) {
+      return NextResponse.json({ error: "تمامی فیلدها الزامی هستند" }, { status: 400 });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await prisma.user.create({
+      data: {
+        firstName,
+        lastName,
+        phone,
+        email,
+        businessType,
+        businessName,
+        password: hashedPassword,
+        role,
+      },
+    });
+
+    return NextResponse.json({
+      success: true,
+      message: "ثبت‌نام موفق. لطفا شماره تلفن را برای دریافت کد OTP ارسال کنید.",
+    });
+  } catch (error:any) {
+    if (error.code === "P2002") {
+      return NextResponse.json({ error: "این ایمیل یا شماره تلفن قبلاً ثبت شده است" }, { status: 400 });
+    }
+
+    console.error("❌ خطا در ثبت نام:", error);
+    return NextResponse.json({ error: "خطا در سرور" }, { status: 500 });
+  }
+}
